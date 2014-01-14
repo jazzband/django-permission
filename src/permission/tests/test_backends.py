bc7e9988c1f5b2a7fe0cb2a8d69c0bdd26fee75a
Λlisue <lambdalisue@hashnote.net>
2014-01-14 09:59:30 +0900

    Add tests for backends

--- src/permission/tests/test_backends.py
+++ src/permission/tests/test_backends.py
@@ -30,3 +30,66 @@ class PermissionPermissionBackendTestCase(TestCase):
     def test_authenticate(self):
         backend = PermissionBackend()
         self.assertEqual(backend.authenticate(None, None), None)
+
+    def test_has_perm_without_obj(self):
+        from permission.utils.handlers import registry
+        _get_handlers = registry.get_handlers
+        perms = [
+            'permission.add_article',
+            'permission.change_article',
+            'permission.delete_article',
+        ]
+        registry.get_handlers = MagicMock(return_value=[
+            MagicMock(get_permissions=MagicMock(return_value=perms),
+                      has_perm=MagicMock(return_value=False)),
+            MagicMock(get_permissions=MagicMock(return_value=perms),
+                      has_perm=MagicMock(return_value=True)),
+        ])
+
+        backend = PermissionBackend()
+        self.assertFalse(registry.get_handlers.called)
+        self.assertFalse(registry.get_handlers()[0].get_permissions.called)
+        self.assertFalse(registry.get_handlers()[1].get_permissions.called)
+        self.assertFalse(registry.get_handlers()[0].has_perm.called)
+        self.assertFalse(registry.get_handlers()[1].has_perm.called)
+
+        self.assertTrue(backend.has_perm(self.user, self.perm1))
+
+        self.assertTrue(registry.get_handlers.called)
+        self.assertTrue(registry.get_handlers()[0].get_permissions.called)
+        self.assertTrue(registry.get_handlers()[1].get_permissions.called)
+        self.assertTrue(registry.get_handlers()[0].has_perm.called)
+        self.assertTrue(registry.get_handlers()[1].has_perm.called)
+
+    def test_has_perm_with_obj(self):
+        from permission.utils.handlers import registry
+        _get_handlers = registry.get_handlers
+        perms = [
+            'permission.add_article',
+            'permission.change_article',
+            'permission.delete_article',
+        ]
+        registry.get_handlers = MagicMock(return_value=[
+            MagicMock(get_permissions=MagicMock(return_value=perms),
+                      has_perm=MagicMock(return_value=False)),
+            MagicMock(get_permissions=MagicMock(return_value=perms),
+                      has_perm=MagicMock(return_value=True)),
+        ])
+
+        backend = PermissionBackend()
+        self.assertFalse(registry.get_handlers.called)
+        self.assertFalse(registry.get_handlers()[0].get_permissions.called)
+        self.assertFalse(registry.get_handlers()[1].get_permissions.called)
+        self.assertFalse(registry.get_handlers()[0].has_perm.called)
+        self.assertFalse(registry.get_handlers()[1].has_perm.called)
+
+        self.assertTrue(backend.has_perm(self.user, self.perm1, self.article))
+
+        self.assertTrue(registry.get_handlers.called)
+        self.assertTrue(registry.get_handlers()[0].get_permissions.called)
+        self.assertTrue(registry.get_handlers()[1].get_permissions.called)
+        self.assertTrue(registry.get_handlers()[0].has_perm.called)
+        self.assertTrue(registry.get_handlers()[1].has_perm.called)
+
+
+
