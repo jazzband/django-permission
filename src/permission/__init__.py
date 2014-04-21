1ae8b5a355cedbe0538c4f483ac3e6e718c29661
Λlisue <lambdalisue@hashnote.net>
2014-04-21 13:56:23 +0900

    Add autodiscover feature

--- src/permission/__init__.py
+++ src/permission/__init__.py
@@ -8,3 +8,9 @@ __version__, VERSION = get_versions('django-permission', allow_ambiguous=True)
 # load shortcut functions
 from permission.utils.logics import add_permission_logic
 from permission.utils.logics import remove_permission_logic
+
+# autodiscover
+from permission.conf import settings
+from permission.utils.autodiscover import autodiscover
+if settings.PERMISSION_AUTODISCOVER_ENABLE:
+    autodiscover()
