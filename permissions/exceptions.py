f5efedd650e077d09bc8505444cb5053ccececfc
Kai Diefenbach <kai.diefenbach@iqpp.de>
2014-06-16 20:38:21 +0200

    Fixes deprecation warning.

--- permissions/exceptions.py
+++ permissions/exceptions.py
@@ -1,3 +1,6 @@
 class Unauthorized(Exception):
-    def __init__(self, str):
-        super(Unauthorized, self).__init__(str)
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return repr(self.message)
