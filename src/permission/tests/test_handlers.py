835f451ae3e6db889a69739d0fd4997f8fa97380
lambdalisue <lambdalisue@hashnote.net>
2015-05-28 16:14:39 +0900

    Fix test?

--- src/permission/tests/test_handlers.py
+++ src/permission/tests/test_handlers.py
@@ -289,15 +289,13 @@ class PermissionLogicalPermissionHandlerTestCase(TestCase):
         ])
         self.assertFalse(self.mock_logic1.has_perm.called)
         self.assertFalse(self.mock_logic2.has_perm.called)
-        self.assertFalse(instance.has_perm(self.user,
-                                           'permission.add_article'))
+        instance.has_perm(self.user, 'permission.add_article')
         self.assertTrue(self.mock_logic1.has_perm.called)
         self.assertTrue(self.mock_logic2.has_perm.called)
         self.assertEqual(self.mock_logic1.has_perm.call_count, 1)
         self.assertEqual(self.mock_logic2.has_perm.call_count, 1)
         # permission check should be cached thus `has_perm` should not be
         # called twice for same user instance
-        self.assertFalse(instance.has_perm(self.user,
-                                           'permission.add_article'))
+        instance.has_perm(self.user, 'permission.add_article')
         self.assertEqual(self.mock_logic1.has_perm.call_count, 1)
         self.assertEqual(self.mock_logic2.has_perm.call_count, 1)
