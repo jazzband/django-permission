6632171d6cc7b8f43c51d01e3969d676f99842d6
lambdalisue <lambdalisue@hashnote.net>
2016-11-24 07:18:47 +0900

    Update manage.py/setup.py and remove runtests.py

--- manage.py
+++ manage.py
@@ -1,30 +1,22 @@
-# coding=utf-8
-"""
-Django 1.2 - 1.6 compatible manage.py
-Modify this script to make your own manage.py
-"""
-__author__ = 'Alisue <lambdalisue@hashnote.net>'
+#!/usr/bin/env python
 import os
 import sys
 
-
-if __name__ == '__main__':
-    # add extra sys.path
-    root = os.path.abspath(os.path.dirname(__file__))
-    extra_paths = (root, os.path.join(root, 'src'))
-    for extra_path in extra_paths:
-        if extra_path in sys.path:
-            sys.path.remove(extra_path)
-        sys.path.insert(0, extra_path)
-    # set DJANGO_SETTINGS_MODULE
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tests.settings')
-
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
     try:
-        # django 1.4 and above
-        # https://docs.djangoproject.com/en/1.4/releases/1.4/
         from django.core.management import execute_from_command_line
-        execute_from_command_line(sys.argv)
     except ImportError:
-        from django.core.management import execute_manager
-        settings = __import__(os.environ['DJANGO_SETTINGS_MODULE'])
-        execute_manager(settings)
+        # The above import may fail for some other reason. Ensure that the
+        # issue is really that Django is missing to avoid masking other
+        # exceptions on Python 2.
+        try:
+            import django
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
+        raise
+    execute_from_command_line(sys.argv)
