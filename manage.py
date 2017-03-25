cad56e15337f6ca194ecff291f5c5df65d0956ba
lambdalisue <lambdalisue@hashnote.net>
2017-03-25 14:49:11 +0900

    Fix coverage test

--- manage.py
+++ manage.py
@@ -4,6 +4,8 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
+    if 'src' not in sys.path:
+        sys.path.insert(0, 'src')
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
